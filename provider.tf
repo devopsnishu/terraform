0ab00c270c2f48ea279cfb87da227a39089ab5ba
devopsnishu <103356069+devopsnishu@users.noreply.github.com>
2023-08-25 13:54:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,5 +8,9 @@ terraform {
 }
 
 provider "azurerm" {
+  subscription_id = "cd14b55f-cd87-48e8-8447-8613523cdfd6"
+  client_id       = "bba666d0-861f-435c-952d-60c304790b55"
+  client_secret   = "SW58Q~Rusa2mwJb3.Z8RKCxYuueIw7Q4RpVXkbFb"
+  tenant_id       = "927e75f9-1770-4a9f-9b71-b4917901d6d6"
   features {}
 }
