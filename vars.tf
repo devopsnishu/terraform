4f5870a11fa6e3c42788f8c6fc0c1fd351f28c5d
devopsnishu <103356069+devopsnishu@users.noreply.github.com>
2023-08-25 14:09:29 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,10 @@
+
+variable "storage_account_name" {
+  description = "The name of the storage account name"
+  default     = "storageaccounttest"
+}
+
+variable "container_name" {
+  description = "The name of the Blob Storage container."
+  default     = "containertest"
+}
